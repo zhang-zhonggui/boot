0534fd0bead351b280db039880bc5df451352ff9
张中贵 <2521250151@qq.com>
2022-05-04 13:35:19 +0800

    测试删除是否能够清除redis缓存

--- src/main/java/com/zzg/boot/controller/StudentController.java
+++ src/main/java/com/zzg/boot/controller/StudentController.java
@@ -1,26 +1,1 @@
-package com.zzg.boot.controller;
-
-
-import com.zzg.boot.pojo.entity.Student;
-import com.zzg.boot.result.AjaxResult;
-import com.zzg.boot.service.StudentService;
-import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-
-/**
- * @author 张中贵
- */
-@RestController
-@Api(tags = "学生管理")
-@RequestMapping("stu")
-public class StudentController {
-    @Autowired
-    private StudentService studentService;
-
-    @GetMapping("queryAllStu")
-    public AjaxResult listStudent() {
-        return studentService.listAllStudent();
-    }
-}
+package com.zzg.boot.controller;import com.zzg.boot.result.AjaxResult;import com.zzg.boot.service.StudentService;import io.swagger.annotations.Api;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.web.bind.annotation.*;/** * @author 张中贵 */@RestController@Api(tags = "学生管理")@RequestMapping("stu")public class StudentController {    @Autowired    private StudentService studentService;    @GetMapping("queryAllStu")    public AjaxResult listStudent() {        return studentService.listAllStudent();    }}
